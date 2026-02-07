85c82b96e8bb35b368eb12d5bb026bdd7891e800
Adithyan Suresh <adithyansuresh161@gmail.com>
2026-02-07 14:44:10 +0530

    Delete scratch directory

--- scratch/js/main.js
+++
@@ -1,6 +0,0 @@
-// Main JS file
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("System Initialized...");
-
-    // Future: Add page transition logic, typing effects, etc.
-});
